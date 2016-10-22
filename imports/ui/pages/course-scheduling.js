c2c722b6be13345a31a797cf043168c725ea7689
David Z. Han <zhaohui.han@gmail.com>
2016-10-21 22:26:06 -0400

    Add linting

--- imports/ui/pages/course-scheduling.js
+++ imports/ui/pages/course-scheduling.js
@@ -37,9 +37,9 @@ class CourseScheduling extends React.Component {
   handleCourseTypeChange = (e, { value }) => this.setState({ courseTypeSelected: value })
   handleRecordsPerPageChange = (e, { value }) => this.setState({ recordsPerPageSelected: value })
   handleSearch = (semesterSelected, courseTypeSelected, recordsPerPageSelected) => {
-    console.log(semesterSelected)
-    console.log(courseTypeSelected)
-    console.log(recordsPerPageSelected)
+    // console.log(semesterSelected)
+    // console.log(courseTypeSelected)
+    // console.log(recordsPerPageSelected)
   }
 
   render() {
