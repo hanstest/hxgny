2a44c8f144956ec5a19ed3c553f7b69df1b82c5a
David Z. Han <zhaohui.han@gmail.com>
2016-10-24 21:15:49 -0400

    fix lint error

--- imports/ui/components/add-new-teacher.js
+++ imports/ui/components/add-new-teacher.js
@@ -1,7 +1,7 @@
 // import { Accounts } from 'meteor/accounts-base'
 import { Meteor } from 'meteor/meteor'
 import React from 'react'
-import { Grid, Button, Header, Form, Message, Confirm } from 'semantic-ui-react'
+import { Grid, Button, Header, Form, Message, Confirm, Input } from 'semantic-ui-react'
 
 class AddNewTeacher extends React.Component {
   state = { serializedForm: {}, open: false, firstName: '' };
@@ -13,11 +13,6 @@ class AddNewTeacher extends React.Component {
   }
   
   handleCancel = () => this.setState({ open: false })
-  
-  handleChange = (e) => {
-    e.preventDefault()
-    this.setState({ email: e.target.value })
-  }
   
   handleSubmit = (e, serializedForm) => {
     e.preventDefault()
@@ -32,28 +27,21 @@ class AddNewTeacher extends React.Component {
     }
   }
   
-  searchUser = (email) => {
-    // TODO Search user in database
-
-  }
-  
   render() {
 
     Meteor.subscribe('allUsers')
     const { serializedForm } = this.state
     return (
-      <Grid textAlign='left' columns={1}>
-        <Grid.Row centered>
+      <Grid textAlign='left' width={16}>
+        <Grid.Row>
           <Grid.Column mobile={16} tablet={16} computer={16}>
             <Header as='h2' icon='add user' content='新教师资料' />
           </Grid.Column>
         </Grid.Row>
         
-        <Grid.Row centered>
-          <Grid.Column>
-  
+        <Grid.Row>
+          <Grid.Column width={8}>
             <Form onSubmit={this.handleSubmit}>
-              
               <Form.Input
                 label='Email'
                 name='email'
@@ -61,17 +49,14 @@ class AddNewTeacher extends React.Component {
                 iconPosition='left'
                 placeholder='david@example.com'
               />
-    
               <Button primary type='submit'>查找账户</Button>
-    
-              <Message>
-                <pre>serializedForm: {JSON.stringify(serializedForm, null, 2)}</pre>
-              </Message>
-  
             </Form>
-
           </Grid.Column>
         </Grid.Row>
+        
+        <Message>
+          <pre>serializedForm: {JSON.stringify(serializedForm, null, 2)}</pre>
+        </Message>
         
         <Confirm
           open={this.state.open}
@@ -80,7 +65,6 @@ class AddNewTeacher extends React.Component {
           onCancel={this.handleCancel}
           onConfirm={this.handleConfirm}
         />
-      
       </Grid>
     )
   }
